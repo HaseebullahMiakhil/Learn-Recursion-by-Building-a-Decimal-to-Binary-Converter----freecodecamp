1bbd657fefc9d6815d6c2d969c078ee16e30c4a7
HaseebullahMiakhil <tw773029@gmail.com>
2024-04-02 11:07:13 +0430

    end of project

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
-
+const animationContainer = document.getElementById("animation-container");
+const animationData = [
+  {
+    inputVal: 5,
+    marginTop: 300,
+    addElDelay: 1000,
+    msg: 'decimalToBinary(5) returns "10" + 1 (5 % 2). Then it pops off the stack.',
+    showMsgDelay: 15000,
+    removeElDelay: 20000,
+  },
+  {
+    inputVal: 2,
+    marginTop: -200,
+    addElDelay: 1500,
+    msg: 'decimalToBinary(2) returns "1" + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.',
+    showMsgDelay: 10000,
+    removeElDelay: 15000,
+  },
+  {
+    inputVal: 1,
+    marginTop: -200,
+    addElDelay: 2000,
+    msg: 'decimalToBinary(1) returns "1" (base case) and gives that value to the stack below. Then it pops off the stack.',
+    showMsgDelay: 5000,
+    removeElDelay: 10000,
+  }
+];
 
 const decimalToBinary = (input) => {
   if (input === 0 || input === 1) {
@@ -12,15 +38,29 @@ const decimalToBinary = (input) => {
 };
 
 const showAnimation = () => {
+  result.innerText = "Call Stack Animation";
+
+  animationData.forEach((obj) => {
+    setTimeout(() => {
+      animationContainer.innerHTML += `
+        <p id="${obj.inputVal}" style="margin-top: ${obj.marginTop}px;" class="animation-frame">
+          decimalToBinary(${obj.inputVal})
+        </p>
+      `;
+    }, obj.addElDelay);
+
+    setTimeout(() => {
+      document.getElementById(obj.inputVal).textContent = obj.msg;
+    }, obj.showMsgDelay);
+
+    setTimeout(() => {
+      document.getElementById(obj.inputVal).remove();
+    }, obj.removeElDelay);
+  });
+
   setTimeout(() => {
-    console.log("free");
-  }, 500);
-  setTimeout(() => {
-    console.log("Code");
-  }, 1000);
-  setTimeout(() => {
-    console.log("Camp");
-  }, 1500);
+result.textContent = decimalToBinary(5);
+  }, 20000);
 };
 
 const checkUserInput = () => {
